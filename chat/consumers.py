20a7c3e158fcc40199686da8d61a02e050aa8bcf
Jason MacFarlane <macfarlane_j@hotmail.com>
2025-01-17 06:52:18 -0700

    Removed import of a model into consumers.py that I removed from the database

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async, aclose_old_connections
-from . models import Chat, Message, ChatGroupCurrentUserCount, ChatViewed
+from . models import Chat, Message, ChatViewed
 from django.contrib.auth.models import User
 from django.db.models import F, Q
 from django.contrib.auth import get_user_model
